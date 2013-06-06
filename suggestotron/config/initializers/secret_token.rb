22827623d1bfc5889bd35cdf16a52c07eb791962
Ken Judy <ken.judy@simonandschuster. com>
2013-06-06 16:44:32 -0400

    getting started - rails scaffold

---
+++ suggestotron/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = 'e892c7877c55ed448154d7e3684a40613a4f8d5c13ca8eee95a63c054d30a8ccfd692dc5c7e7b5302a5485d82f974a05c0e9ec9aa5e18d4c0904b6266daa52a8'
